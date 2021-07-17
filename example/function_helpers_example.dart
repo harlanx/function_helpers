f635047a71100c59b181eb2b2383b4812e21403f
harlanx <silan.harry@gmail.com>
2021-07-18 03:02:14 +0800

    moved math methods to extension utils

--- example/function_helpers_example.dart
+++ example/function_helpers_example.dart
@@ -1,4 +1,7 @@
-//import 'package:function_helpers/function_helpers.dart';
+import 'package:function_helpers/function_helpers.dart';
 
 void main() {
+  var x = 50;
+  x.valToNewRange(oldMin: 0, oldMax: 100, newMin: -1, newMax: 1);
+  print(x);
 }
--- lib/src/extension_helper.dart
+++ lib/src/extension_helper.dart
@@ -15,7 +15,6 @@ extension IterableNumUtil<T extends num> on Iterable<num> {
 }
 
 extension IterableUtil on Iterable {
-
   /// Removes the duplicate of an element that has more than 1 occurence.
   List get removeDuplicate => toSet().toList();
 
@@ -31,11 +30,29 @@ extension IterableUtil on Iterable {
   }
 
   /// Returns a Map frequency count of each element in a list.
-  /// 
+  ///
   /// The elements are used as keys.
-  Map<dynamic, int> frequencies(){
+  Map<dynamic, int> frequencies() {
     var map = <dynamic, int>{};
     forEach((e) => map.update(e, (x) => (x) + 1, ifAbsent: () => 1));
     return map;
+  }
+}
+
+extension NumUtil<T extends num> on T {
+
+  /// Scales this value into a new value while maintaining the correct ratio between the range of [newMin] and [newMax].
+  T valToNewRange({
+    required T oldMin,
+    required T oldMax,
+    required T newMin,
+    required T newMax,
+  }) {
+    var isInRange = this >= oldMin && this <= oldMax;
+    if (!isInRange) throw RangeError('current value must be in between the oldMin and oldMax');
+
+    //Linear Conversion
+    var newValue = (((this - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
+    return newValue as T;
   }
 }
